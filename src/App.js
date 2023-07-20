cd16d7184962f4faf6881f85f9498cfd372afb66
Pramada Shrestha <pramadashrestha17@gmail.com>
2023-07-20 10:22:01 +0545

    delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import Header from "./component/Header";
+import Tasks from "./component/Tasks";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors appointment",
+      day: "Dec 19 at 7am",
+      reminder: true,
+    },
+
+    {
+      id: 2,
+      text: "Dentist appointment",
+      day: "Jan 11 at 9am",
+      reminder: true,
+    },
+
+    {
+      id: 3,
+      text: "Meeting",
+      day: "May 7 at 8:30am",
+      reminder: false,
+    },
+  ]);
+
+  // Delete tasks
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+    console.log("delete", id);
+  };
+
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
-}
+};
 
 export default App;
